7122ac3c5f8876218a622f7b15393b5cffc14424
Alex <alex1337webdev@gmail.com>
2024-02-08 20:44:38 +0500

    секция с вкладками

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,47 @@ document.querySelector(".header__burger--icon").addEventListener("click", functi
   document.querySelector(".header__nav").classList.toggle("active");
   document.querySelector("body").classList.toggle("lock");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Перенос заголовка для мобильных устройств
+    const moveTitleForMobile = () => {
+        const advantages = document.querySelector('.advantages');
+        if (advantages && window.innerWidth < 768) {
+            const advantagesTitle = advantages.querySelector('.advantages__content--title');
+            if (advantagesTitle) {
+                const newRow = document.createElement('div');
+                newRow.classList.add('row');
+                newRow.appendChild(advantagesTitle.cloneNode(true)); // Клонируем заголовок
+                advantagesTitle.remove(); // Удаляем оригинальный заголовок после клонирования
+
+                const container = advantages.querySelector('.container');
+                if (container) {
+                    container.insertBefore(newRow, container.firstChild);
+                }
+            }
+        }
+    };
+
+    // Переносим заголовок при загрузке страницы
+    moveTitleForMobile();
+
+    // Обработчик кликов для переключения вкладок
+    const tabsContainer = document.querySelector('.advantages');
+    if (tabsContainer) {
+        tabsContainer.addEventListener('click', function(e) {
+            const target = e.target.closest('.tab-item'); // Используем closest для увеличения гибкости
+            if (target) {
+                const dataItem = target.getAttribute('data-item');
+                const tabItems = document.querySelectorAll('.advantages .tab-item');
+                const tabContents = document.querySelectorAll('.advantages .tab-content');
+
+                tabItems.forEach(item => item.classList.remove('active'));
+                tabContents.forEach(content => content.classList.remove('active'));
+
+                target.classList.add('active');
+                document.querySelectorAll(`.advantages .tab-content[data-tab="${dataItem}"]`).forEach(item => item.classList.add('active'));
+            }
+        });
+    }
+});
+

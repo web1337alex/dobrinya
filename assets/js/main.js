6fae655840ce5beacec236614c50561e81a5e442
Alex <alex1337webdev@gmail.com>
2024-02-20 01:16:23 +0500

    мелкие правки

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,21 +28,23 @@ document.addEventListener('DOMContentLoaded', function() {
     moveTitleForMobile();
 
     // Обработчик кликов для переключения вкладок
-    const tabsContainer = document.querySelector('.advantages');
-    if (tabsContainer) {
-        tabsContainer.addEventListener('click', function(e) {
-            const target = e.target.closest('.tab-item'); // Используем closest для увеличения гибкости
-            if (target) {
-                const dataItem = target.getAttribute('data-item');
-                const tabItems = document.querySelectorAll('.advantages .tab-item');
-                const tabContents = document.querySelectorAll('.advantages .tab-content');
+    const tabsContainers = document.querySelectorAll('.tab__container');
+    if (tabsContainers) {
+        tabsContainers.forEach(tabsContainer => {
+            tabsContainer.addEventListener('click', function(e) {
+                const target = e.target.closest('.tab-item'); // Используем closest для увеличения гибкости
+                if (target) {
+                    const dataItem = target.getAttribute('data-item');
+                    const tabItems = document.querySelectorAll('.tab__container .tab-item');
+                    const tabContents = document.querySelectorAll('.tab__container .tab-content');
 
-                tabItems.forEach(item => item.classList.remove('active'));
-                tabContents.forEach(content => content.classList.remove('active'));
+                    tabItems.forEach(item => item.classList.remove('active'));
+                    tabContents.forEach(content => content.classList.remove('active'));
 
-                target.classList.add('active');
-                document.querySelectorAll(`.advantages .tab-content[data-tab="${dataItem}"]`).forEach(item => item.classList.add('active'));
-            }
+                    target.classList.add('active');
+                    document.querySelectorAll(`.tab__container .tab-content[data-tab="${dataItem}"]`).forEach(item => item.classList.add('active'));
+                }
+            });
         });
     }
 });
